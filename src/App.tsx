57591b517d94120284b35cef7d79f26b161f8fb1
Giselle Perez <giselle.perez@aerolab.com>
2021-09-17 03:26:03 -0300

    feat: add basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
 
 import Home from './pages/Home';
 import Film from './pages/Film';
@@ -9,15 +10,15 @@ function App() {
     <div className="App">
       <div>
         <Router>
-          <header>Header</header>
+          <header>
+            <h1>Star Wars Movies</h1>
+          </header>
 
           <Switch>
             <Route exact path="/echarts-sw" component={Home} />
             <Route path="/echarts-sw/films/:id" component={Film} />
           </Switch>
         </Router>
-
-        <footer>Footer</footer>
       </div>
     </div>
   );
